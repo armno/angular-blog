d8c4ea1c81ad058d08be28e487548d226c58f011
Armno <armno@buzzwoo.de>
2013-09-27 16:05:44 +0700

    created Post custom service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,24 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('PostListController', ['$scope', '$http', function($scope, $http) {
+  controller('PostListController', ['$scope', '$http', 'Post', function($scope, $http, Post) {
 		// get posts data from posts/all.json
-		$http({
-			method: 'GET',		
-			url: 'posts/all.json' 
-		}).success(function(posts) {
-		  $scope.posts = posts;	
-		});
+//		$http({
+//			method: 'GET',		
+//			url: 'posts/all.json' 
+//		}).success(function(posts) {
+//		  $scope.posts = posts;	
+//		});
+		$scope.posts = Post.query();
   }])
-  .controller('PostDetailController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  .controller('PostDetailController', ['$scope', '$routeParams', '$http', 'Post', function($scope, $routeParams, $http, Post) {
 		// get post content from slug
-		$http
-			.get('posts/' + $routeParams.postId + '.json')
-			.success(function(post) {
-				$scope.post = post
-			});
+//		$http
+//			.get('posts/' + $routeParams.postId + '.json')
+//			.success(function(post) {
+//				$scope.post = post
+//			});
+		$scope.post = Post.get({
+			postId: $routeParams.postId
+		});
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+angular.module('myApp.services', ['ngResource']).
+	factory('Post', function($resource) {
+		return $resource('posts/:postId.json', {}, {
+			query: {
+				method: 'GET',
+				params: {
+					postId: 'posts' // default :postId value
+				},
+				isArray: true
+			}
+		});		
+	}).
   value('version', '0.1');
