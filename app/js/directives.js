38745d2ce040ca48c73206e63e3acfdfd751cc9d
ArmNo <monkeyarmno@gmail.com>
2013-10-07 17:07:16 +0700

    finally create my first directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,14 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+	.directive('fbImg', [function() {
+		// usage <fb-img data-fb-id="armno">
+		return {
+			restrict: 'E',
+			link: function(scope, element, attr) {
+				var fbId = attr.fbId;
+				element.append('<img src="//graph.facebook.com/' + fbId + '/picture">');
+			}
+		};
+	}]);
